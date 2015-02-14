ef97d0018d5877c71b5727580c1f12ea2a343c06
Peter Burkholder <peterb@getchef.com>
2015-02-14 16:43:17 -0500

    Specific to CrossplatThing

--- unit/crossplat_thing_spec.rb
+++ unit/crossplat_thing_spec.rb
@@ -1,8 +1,8 @@
 require_relative 'spec_helper'
 describe Chef::Provider::CrossplatThing do
+  let(:new_resource) { Chef::Resource::CrossplatThing.new('') }
   let(:node) { Chef::Node.new() }
   let(:run_context) { Chef::RunContext.new(node,{},nil) }
-  let(:new_resource) { Chef::Resource.new('file') }
   subject(:provider) {
     Chef::Provider::CrossplatThing.new(new_resource, run_context)
   }
--- unit/spec_helper.rb
+++ unit/spec_helper.rb
@@ -1,8 +1,5 @@
 require 'chef/platform'
 require 'chef/run_context'
-require 'chef/resource'
-require 'chef/provider'
-require 'chef/provider/lwrp_base'
-require 'chef/resource/lwrp_base'
 
 require_relative '../libraries/provider_crossplat_thing'
+require_relative '../libraries/resource_crossplat_thing'
