4bf735e7699519ea6f20e94564e8e0a0fa4f5344
Peter Burkholder <peterb@getchef.com>
2015-02-14 16:34:55 -0500

    It should be is true

--- unit/crossplat_thing_spec.rb
+++ unit/crossplat_thing_spec.rb
@@ -3,5 +3,11 @@ describe Chef::Provider::CrossplatThing do
   let(:node) { Chef::Node.new() }
   let(:run_context) { Chef::RunContext.new(node,{},nil) }
   let(:new_resource) { Chef::Resource.new('file') }
-  subject(:provider) { Chef::Provider::CrossplatThing.new(new_resource, run_context)}
+  subject(:provider) {
+    Chef::Provider::CrossplatThing.new(new_resource, run_context)
+  }
+
+  it do
+    provider.should be
+  end
 end
